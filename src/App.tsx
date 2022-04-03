9dd235f660d1095684869a03b1870f5619bb6eec
xavier-kong <kongxianchengxavier@gmail.com>
2022-04-03 13:20:00 +0800

    added background color

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
+const appStyle = {
+    background: '#1e1e1e',
+    color: '#d4d4d4',
+};
+
 function App() {
-    return <div></div>;
+    return <div style={appStyle}>Hello</div>;
 }
 
 export default App;
