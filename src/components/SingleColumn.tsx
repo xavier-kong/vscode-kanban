9b351a230a29d470c1ecf94fd332a9622df9c11e
xavier-kong <kongxianchengxavier@gmail.com>
2022-04-04 19:38:48 +0800

    created singlecolumn component

---
+++ src/components/SingleColumn.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+function SingleColumn() {}
+
+export default SingleColumn;
--- src/views/Kanban.tsx
+++ src/views/Kanban.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CreateColumnButton from '../components/CreateColumnButton';
+import SingleColumn from '../components/SingleColumn';
 
 function Kanban() {
+    const [columns, setColumns] = useState([]);
     function createColumn() {}
     return (
         <div>
