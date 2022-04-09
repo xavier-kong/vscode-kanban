8fa8d0b8d9a2b56486bcb0599300223674bc0a80
xavier-kong <kongxianchengxavier@gmail.com>
2022-04-09 17:23:37 +0800

    adding styles to colum header input

--- src/components/ColumnHeader.tsx
+++ src/components/ColumnHeader.tsx
@@ -1,10 +1,32 @@
 import { Container } from '@mui/material';
-import React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { styled } from '@mui/material/styles';
 
 interface propTypes {
     name: string;
     status: string;
 }
+
+const CssTextField = styled(TextField)({
+    '& label.Mui-focused': {
+        color: 'white',
+    },
+    '& .MuiInput-underline:after': {
+        borderBottomColor: 'white',
+    },
+    '& .MuiOutlinedInput-root': {
+        '& fieldset': {
+            borderColor: 'white',
+        },
+        '&:hover fieldset': {
+            borderColor: 'white',
+        },
+        '&.Mui-focused fieldset': {
+            borderColor: 'white',
+        },
+    },
+});
 
 function ColumnHeader({ name, status }: propTypes) {
     /* 
@@ -13,7 +35,31 @@ function ColumnHeader({ name, status }: propTypes) {
         add listner on click away or enter or click save icon to save name
         logic to save name
     */
-    return <Container sx={{ marginTop: '10px' }}>{name}</Container>;
+    return (
+        <Container sx={{ marginTop: '10px' }}>
+            {status === 'new' ? (
+                <Box
+                    component="form"
+                    // sx={
+                    //     {
+                    //         '& > :not(style)': { m: 1, width: '25ch' },
+                    //     }
+                    // }
+                    noValidate
+                    autoComplete="off"
+                >
+                    <CssTextField
+                        label="Enter Column Name"
+                        id="custom-css-outlined-input"
+                        variant="standard"
+                        autoFocus={true}
+                    />
+                </Box>
+            ) : (
+                name
+            )}
+        </Container>
+    );
 }
 
 export default ColumnHeader;
