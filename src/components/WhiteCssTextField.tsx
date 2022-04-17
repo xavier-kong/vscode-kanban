f13b560c148f60d73116193084587ae43c03c858
xavier-kong <kongxianchengxavier@gmail.com>
2022-04-17 17:15:23 +0800

    forgot to git add lol

---
+++ src/components/WhiteCssTextField.tsx
@@ -0,0 +1,27 @@
+import TextField from '@mui/material/TextField';
+import { styled } from '@mui/material/styles';
+
+const WhiteCssTextField = styled(TextField)({
+    '& label.Mui-focused': {
+        color: 'white',
+    },
+    '& .MuiInput-underline:after': {
+        borderBottomColor: 'white',
+    },
+    '& .MuiInput-underline:before': {
+        borderBottomColor: 'white',
+    },
+    '& .MuiOutlinedInput-root': {
+        '& fieldset': {
+            borderColor: 'white',
+        },
+        '&:hover fieldset': {
+            borderColor: 'white',
+        },
+        '&.Mui-focused fieldset': {
+            borderColor: 'white',
+        },
+    },
+});
+
+export default WhiteCssTextField;
