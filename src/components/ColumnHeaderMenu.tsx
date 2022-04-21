2c90c5f34599a90a80f7d877ce0b9106a35dad6f
xavier-kong <kongxianchengxavier@gmail.com>
2022-04-21 22:38:43 +0800

    changed menu item names

--- src/components/ColumnHeaderMenu.tsx
+++ src/components/ColumnHeaderMenu.tsx
@@ -3,7 +3,6 @@ import MenuItem from '@mui/material/MenuItem';
 import MenuList from '@mui/material/MenuList';
 import ListItemText from '@mui/material/ListItemText';
 import ListItemIcon from '@mui/material/ListItemIcon';
-import Typography from '@mui/material/Typography';
 import ContentCut from '@mui/icons-material/ContentCut';
 import ContentCopy from '@mui/icons-material/ContentCopy';
 import ContentPaste from '@mui/icons-material/ContentPaste';
@@ -31,19 +30,25 @@ function ColumnHeaderMenu({ anchorEl, open, handleClose }: PropTypes) {
                     <ListItemIcon>
                         <ContentCut fontSize="small" />
                     </ListItemIcon>
-                    <ListItemText>Cut</ListItemText>
+                    <ListItemText>Rename Column</ListItemText>
                 </MenuItem>
                 <MenuItem>
                     <ListItemIcon>
                         <ContentCopy fontSize="small" />
                     </ListItemIcon>
-                    <ListItemText>Copy</ListItemText>
+                    <ListItemText>Change Colour</ListItemText>
                 </MenuItem>
                 <MenuItem>
                     <ListItemIcon>
                         <ContentPaste fontSize="small" />
                     </ListItemIcon>
-                    <ListItemText>Paste</ListItemText>
+                    <ListItemText>Delete Column</ListItemText>
+                </MenuItem>
+                <MenuItem>
+                    <ListItemIcon>
+                        <ContentPaste fontSize="small" />
+                    </ListItemIcon>
+                    <ListItemText>Hide Column</ListItemText>
                 </MenuItem>
             </MenuList>
         </Menu>
