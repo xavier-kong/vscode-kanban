630a8fd0ee219dec63deab87af384d7e3ae2de81
xavier-kong <kongxianchengxavier@gmail.com>
2022-05-01 17:18:06 +0800

    added hidden column menu button

--- src/components/HiddenColumns.tsx
+++ src/components/HiddenColumns.tsx
@@ -1,3 +1,9 @@
+import { useState } from 'react';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+
 interface Column {
     name: string;
     default: boolean;
@@ -10,8 +16,60 @@ interface PropTypes {
     columns: Column[];
 }
 
+const style = {
+    color: '#FFF',
+    border: 'none',
+};
+
 function HiddenColumns({ columns }: PropTypes) {
-    return <div>HELLo!</div>;
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: any) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
+        <Container>
+            <Button
+                id="basic-button"
+                aria-controls={open ? 'basic-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+                onClick={handleClick}
+                style={style}
+            >
+                Hidden Columns
+            </Button>
+            <Button
+                variant="text"
+                // startIcon={<AddIcon />}
+                style={style}
+                disableRipple={false}
+                onClick={(e) => {
+                    e.preventDefault();
+                    // createColumn();
+                }}
+            >
+                Add Column
+            </Button>
+            <Menu
+                id="basic-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                    'aria-labelledby': 'basic-button',
+                }}
+            >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+                <MenuItem onClick={handleClose}>Logout</MenuItem>
+            </Menu>
+        </Container>
+    );
 }
 
 export default HiddenColumns;
