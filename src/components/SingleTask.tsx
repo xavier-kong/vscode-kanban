f01117dfd9c1ecdac445a2c096bfa2dacea046ef
xavier-kong <kongxianchengxavier@gmail.com>
2022-06-04 21:24:33 +0800

    SILENCED ANOTHER TYPESCRIPT ERROR

--- src/components/SingleTask.tsx
+++ src/components/SingleTask.tsx
@@ -1,3 +1,11 @@
-function SingleTask() {}
+import Tasks from '../types/Tasks';
+
+interface PropTypes {
+    task: Tasks;
+}
+
+function SingleTask({ task }: PropTypes) {
+    return <div></div>;
+}
 
 export default SingleTask;
